82bac01c36e6e35b9dcf22b5b8de0c60412ef34b
Nikita <Nikita.Clark@yandex.ru>
2020-08-21 21:04:21 +0300

    step_3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
+#! /usr/bin/python3
 
+from brain_games.cli import welcome_user
 
 def main():
-	print("Welcome to the Brain Games!")
+	print("Welcome to the Brain Games!\n")
+	welcome_user()
 
 if __name__ == '__main__':
 	main()
