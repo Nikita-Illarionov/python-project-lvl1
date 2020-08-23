77be0a5148aab1278f0f1cf7879e1f750a625330
Nikita <Nikita.Clark@yandex.ru>
2020-08-23 18:46:21 +0300

    step_5

--- brain_games/brain_even_body.py
+++ brain_games/brain_even_body.py
@@ -3,7 +3,8 @@ import random
 
 
 def body():
-    print('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n')
+    print('Welcome to the Brain Games!\nAnswer "yes"\
+ if number even otherwise answer "no".\n')
     name = prompt.string('May I have your name? ')
     print('\n')
     n = 0
@@ -18,10 +19,12 @@ def body():
         if answer == right_answer:
             print('Correct!')
         elif answer == 'yes' or answer == 'no':
-            print('"{}" is wrong answer ;(. Correct answer was "{}".Let\'s try again, {}!'.format(answer, right_answer, name))
+            print('"{}" is wrong answer ;(. Correct answer was "{}".\
+Let\'s try again, {}!'.format(answer, right_answer, name))
             break
         else:
-            print('Incorrect answer: you should write just "yes" or "no". Let\'s try again, {}!'.format(name))
+            print('Incorrect answer: you should write just "yes" or "no".\
+ Let\'s try again, {}!'.format(name))
             break
         n += 1
     if n == 3:
