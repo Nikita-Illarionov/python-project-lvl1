cb98f01bd0df0718a7af88ecc84cdb77efc76dd3
Nikita <Nikita.Clark@yandex.ru>
2020-08-25 12:42:18 +0300

    step_8

--- brain_games/games/main_brain_progression.py
+++ brain_games/games/main_brain_progression.py
@@ -4,8 +4,8 @@ import prompt
 
 def f():
     step = random.randint(1, 10)
-    tmp = random.randint(1, 100) #first number
-    k = random.randint(1, 10) #number of the hidden element
+    tmp = random.randint(1, 100)  # first number
+    k = random.randint(1, 10)  # number of the hidden element
     right_answer = tmp + step * (k-1)
     print('Question: ', end='')
     for i in range(1, 11):
